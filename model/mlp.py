8079564a905da5e61facd16300f2d8caf22c61c3
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-04-13 22:16:08 +0100

    forward-propagating : mlp

---
+++ model/mlp.py
@@ -0,0 +1,9 @@
+
+class MLP:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def forward(self, X):
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
