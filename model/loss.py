6e48f284ca585c8839a86cf0e2231e87edd7a67b
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-04-25 01:50:26 +0100

    implement Mean Squared Error gradient function

--- model/loss.py
+++ model/loss.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def mse(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2) # Mean Squared Error
+
+def mse_grad(y_true, y_pred):
+    n = y_true.shape[0]
+    return (2 / n) * (y_pred - y_true)
