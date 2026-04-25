ce8a505046eea21102f000faeb021cb6e28d1d41
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-04-25 01:47:45 +0100

    adding backward for ReLU and Linear + introducing Tanh activation

--- model/activations.py
+++ model/activations.py
@@ -5,8 +5,24 @@ class ReLU:
     def forward(self, Z):
         self.Z = Z # store for future backprop
         return np.maximum(0, Z) 
+    
+    def backward(self, dA):
+        dZ = dA * (self.Z > 0)
+        return dZ
 
 
 class Linear:
     def forward(self, Z):
-        return Z 
+        return Z
+
+    def backward(self, dA):
+        return dA 
+    
+class Tanh:
+    def forward(self, Z):
+        self.A = np.tanh(Z)
+        return self.A
+
+    def backward(self, dA):
+        dZ = dA * (1 - self.A**2)
+        return dZ    
