11448ac43d954870680997d64313a63e75414ca4
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-04-29 23:19:33 +0100

    Add mini-batch training to Trainer

--- training/trainer.py
+++ training/trainer.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 from model.loss import mse, mse_grad
 
@@ -8,27 +9,36 @@ class Trainer:
         self.model = model
         self.optimizer = optimizer
 
-    def train(self, X, y, epochs=1000, plot_fn=None):
+    def train(self, X, y, epochs=1000, batch_size=32, plot_fn=None):
         losses = []
         ax = None
+        n_samples = X.shape[0]
 
         for epoch in range(epochs):
-            # Forward
-            y_pred = self.model.forward(X)
+            indices = np.random.permutation(n_samples)
+            X_shuffled = X[indices]
+            y_shuffled = y[indices]
 
-            # Loss
-            loss = mse(y, y_pred)
-            losses.append(loss)
+            epoch_losses = []
 
-            # Backward
-            grad = mse_grad(y, y_pred)
-            self.model.backward(grad)
+            for start in range(0, n_samples, batch_size):
+                end = start + batch_size
+                X_batch = X_shuffled[start:end]
+                y_batch = y_shuffled[start:end]
 
-            # Update
-            self.optimizer.step()
+                y_pred = self.model.forward(X_batch)
+                loss = mse(y_batch, y_pred)
+                epoch_losses.append(loss)
+
+                grad = mse_grad(y_batch, y_pred)
+                self.model.backward(grad)
+                self.optimizer.step()
+
+            epoch_loss = float(np.mean(epoch_losses))
+            losses.append(epoch_loss)
 
             if epoch % 100 == 0:
-                print(f"Epoch {epoch}, Loss: {loss}")
+                print(f"Epoch {epoch}, Loss: {epoch_loss}")
 
             if plot_fn and epoch % 20 == 0:
                 ax = plot_fn(losses, ax, live=True)
